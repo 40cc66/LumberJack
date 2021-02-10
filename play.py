ea821648dd568fe2589ea825aa8676344979c85a
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-02-10 11:51:19 +0200

    Even more faster

--- play.py
+++ play.py
@@ -1,4 +1,9 @@
-﻿import sys
+﻿"""
+Author: Stanislav Akimov
+"""
+import sys
+from itertools import product
+
 import pyautogui
 
 pyautogui.click(400, 400)  # Move to the game window.
@@ -7,26 +12,25 @@ branch_colors = [
     (161, 116, 56),
     (136, 99, 50),
 ]  # Branch colors found out with `python -m pyautogui`.
-chop_left = True  # Start chopping from left.
-while True:
-    if chop_left:
-        pyautogui.press('left', interval=interval)
-        pixel = pyautogui.pixel(425, 645)
-        if pixel in branch_colors:
-            pyautogui.press('right', interval=0)
-            chop_left = False
-        else:
-            pixel = pyautogui.pixel(535, 645)
-            if pixel in branch_colors:
-                pyautogui.press('left', interval=0)
 
-    else:
-        pyautogui.press('right', interval=interval)
-        pixel = pyautogui.pixel(535, 645)
+LEFT = 'left'
+RIGHT = 'right'
+
+branch_ys = (645, 545, 445, 345, 245, 145)
+left_branch_x = 430
+right_branch_x = 530
+
+pyautogui.press('left')
+pyautogui.press('left')
+
+while True:
+    pyautogui.sleep(interval)
+    chops = []
+    for y, x in product(branch_ys, (left_branch_x, right_branch_x)):
+        pixel = pyautogui.pixel(x, y)
         if pixel in branch_colors:
-            pyautogui.press('left', interval=0)
-            chop_left = True
-        else:
-            pixel = pyautogui.pixel(425, 645)
-            if pixel in branch_colors:
-                pyautogui.press('right', interval=0)
+            chop_side = RIGHT if x == left_branch_x else LEFT
+            chops.append(chop_side)
+    for chop_side in chops:
+        pyautogui.press(chop_side)
+        pyautogui.press(chop_side)
