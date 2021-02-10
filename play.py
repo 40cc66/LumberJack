5f2dfbc169b0cd079a503e3cb71810bc418ddc7c
Stanislav Akimov <39214862+akimovstv@users.noreply.github.com>
2021-02-10 09:44:49 +0200

    Сhop evenly

--- play.py
+++ play.py
@@ -1,7 +1,8 @@
+import sys
 import pyautogui
 
 pyautogui.click(400, 400)  # Move to the game window.
-interval = 0.08
+interval = float(sys.argv[-1]) if len(sys.argv) == 2 else 0.07
 branch_colors = [
     (161, 116, 56),
     (136, 99, 50),
@@ -12,20 +13,10 @@ while True:
         pyautogui.press('left', interval=interval)
         pixel = pyautogui.pixel(425, 645)
         if pixel in branch_colors:
-            pyautogui.press('right', interval=0)
             chop_left = False
-        else:
-            pixel = pyautogui.pixel(535, 645)
-            if pixel in branch_colors:
-                pyautogui.press('left', interval=0)
 
     else:
         pyautogui.press('right', interval=interval)
         pixel = pyautogui.pixel(535, 645)
         if pixel in branch_colors:
-            pyautogui.press('left', interval=0)
             chop_left = True
-        else:
-            pixel = pyautogui.pixel(425, 645)
-            if pixel in branch_colors:
-                pyautogui.press('right', interval=0)
